219413dd5efa90aafc0a780953707c5fb5bee603
Chippalrus <chippalrus.dev@gmail.com>
2024-10-18 21:26:28 -0700

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  base: 'cv/'
+  integrations: [tailwind()]
+  , base: '/cv/'
 });
